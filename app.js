9c137f9f8015db33dd43026a39ab0834d40cd0dd
Victoria <vickimarji@icloud.com>
2022-07-12 21:12:59 -0400

    please work

---
+++ app.js
@@ -0,0 +1,43 @@
+var x = 5;
+var y = 4;
+
+
+function sum(param1, param2) {
+    console.log(param1 + param2);
+}
+
+sum(x, y);
+
+
+var x = 5;
+var y = 4;
+
+function sum(param1, param2) {
+    console.log(param1 - param2);
+
+}
+
+sum(x, y);
+
+
+var x = 5;
+var y = 4;
+
+function sum(param1, param2) {
+console.log(param1 * param2);
+
+}
+
+sum(x, y);
+
+
+
+var x = 5;
+var y = 4;
+
+function sum(param1, param2) {
+console.log(param1 / param2);
+
+}
+
+sum(x, y);
